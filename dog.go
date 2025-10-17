4a957dd382bf9763805c5fa3feeab0c28f22fa57
KiraTeresa <kira.soba@gmx.de>
2025-10-17 07:45:34 +0200

    testing versions

--- dog.go
+++ dog.go
@@ -1,9 +1,14 @@
 package dog
 
 import (
+	"fmt"
 	"strings"
 )
 
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
+}
+
+func From11() {
+	fmt.Println("Dog repo v1.1.0")
 }
